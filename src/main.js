ccacadedaef459080bb248864f089ba6543eb2b7
Marshall Shen <shen.marshall@gmail.com>
2016-09-05 14:55:22 -0500

    Step 5: create components for news board and each news

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { domain, fromNow } from './filters'
 import App from './components/App.vue'
+import NewsView from './components/NewsView.vue'
 
 // install router
 Vue.use(Router)
@@ -12,5 +13,20 @@ Vue.filter('domain', domain)
 
 // routing
 var router = new Router()
+
+router.map({
+  '/news/:page': {
+    component: NewsView
+  }
+})
+
+
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
+
+router.redirect({
+  '*': '/news/1'
+})
 
 router.start(App, '#app')
